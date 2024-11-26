579e1ab59b5c209e97b48a1f7486a6b7032a6791
artl <artl@microsoft.com>
2024-11-26 09:48:59 -0800

    WIP: Simple console app

--- src/windows/Program.cs
+++ src/windows/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Win32;
+
+System.DateTime startTime = System.DateTime.Now;
+System.TimeSpan downTime = System.TimeSpan.Zero;
+System.DateTime now = System.DateTime.Now;
+
+void SystemEvents_SessionSwitch(object sender, SessionSwitchEventArgs e)
+{
+    if (e.Reason == SessionSwitchReason.SessionLock)
+    {
+        downTime = System.DateTime.Now - now;
+        Console.WriteLine("The session has been locked.");
+    }
+    else if (e.Reason == SessionSwitchReason.SessionUnlock)
+    {
+        Console.WriteLine("The session has been unlocked.");
+        Console.WriteLine("The session was locked for " + downTime.TotalSeconds + " seconds.");
+    }
+}
+
+SystemEvents.SessionSwitch += new SessionSwitchEventHandler(SystemEvents_SessionSwitch);
+Console.WriteLine("Listening for session switch events. Press any key to exit...");
+
+await Task.Run(async () =>
+{
+    while (true)
+    {
+        now = DateTime.Now;
+        await Task.Delay(1000);
+        Console.WriteLine("Time logged in interactively: " + (now - startTime - downTime).ToString());
+    }
+});
+
+ 
+
