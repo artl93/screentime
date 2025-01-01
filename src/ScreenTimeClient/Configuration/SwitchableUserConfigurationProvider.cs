d05746d01ebcbbfcad493142c55b13e3dc13ab47
Art Leonard <artl@microsoft.com>
2024-12-31 17:45:42 -0800

    Rename fields

--- src/ScreenTimeClient/Configuration/SwitchableUserConfigurationProvider.cs
+++ src/ScreenTimeClient/Configuration/SwitchableUserConfigurationProvider.cs
@@ -5,65 +5,65 @@ namespace ScreenTimeClient.Configuration
 {
     public class SwitchableUserConfigurationProvider : IUserConfigurationProvider, IDisposable
     {
-        private readonly LocalUserConfigurationProvider backupProvider;
-        private readonly RemoteUserConfigurationProvider provider;
+        private readonly LocalUserConfigurationProvider localProvider;
+        private readonly RemoteUserConfigurationProvider remoteProvider;
         public event EventHandler<UserConfigurationEventArgs>? OnConfigurationChanged;
         public event EventHandler<UserConfigurationResponseEventArgs>? OnExtensionResponse;
-        public SwitchableUserConfigurationProvider(RemoteUserConfigurationProvider provider, LocalUserConfigurationProvider backupProvider)
+        public SwitchableUserConfigurationProvider(RemoteUserConfigurationProvider remoteProvider, LocalUserConfigurationProvider localProvider)
         {
-            this.provider = provider;
-            this.backupProvider = backupProvider;
-            provider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
-            provider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
+            this.remoteProvider = remoteProvider;
+            this.localProvider = localProvider;
+            remoteProvider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
+            remoteProvider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
         }
         public async Task<UserConfiguration> GetUserConfigurationForDayAsync()
         {
-            // always enable remote provider
-            var result = await backupProvider.GetUserConfigurationForDayAsync();
+            // always enable remote remoteProvider
+            var result = await localProvider.GetUserConfigurationForDayAsync();
             var newResult = result with { EnableOnline = true };
             return newResult;
         }
         public Task SaveUserConfigurationForDayAsync(UserConfiguration configuration)
         {
-            return backupProvider.SaveUserConfigurationForDayAsync(configuration);
+            return localProvider.SaveUserConfigurationForDayAsync(configuration);
         }
         public void ResetExtensions()
         {
-            backupProvider.ResetExtensions();
+            localProvider.ResetExtensions();
         }
         public void AddExtension(DateTimeOffset date, int minutes)
         {
-            backupProvider.AddExtension(date, minutes);
+            localProvider.AddExtension(date, minutes);
         }
 
         public void SwitchToBackup()
         {
-            if (backupProvider is null)
+            if (localProvider is null)
             {
                 return;
             }
-            provider.OnConfigurationChanged -= (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
-            provider.OnExtensionResponse -= (sender, args) => OnExtensionResponse?.Invoke(sender, args);
-            backupProvider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
-            backupProvider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
+            remoteProvider.OnConfigurationChanged -= (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
+            remoteProvider.OnExtensionResponse -= (sender, args) => OnExtensionResponse?.Invoke(sender, args);
+            localProvider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
+            localProvider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
         }
         public void SwitchToPrimary()
         {
-            if (backupProvider is null)
+            if (localProvider is null)
             {
                 return;
             }
-            backupProvider.OnConfigurationChanged -= (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
-            backupProvider.OnExtensionResponse -= (sender, args) => OnExtensionResponse?.Invoke(sender, args);
-            provider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
-            provider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
+            localProvider.OnConfigurationChanged -= (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
+            localProvider.OnExtensionResponse -= (sender, args) => OnExtensionResponse?.Invoke(sender, args);
+            remoteProvider.OnConfigurationChanged += (sender, args) => OnConfigurationChanged?.Invoke(sender, args);
+            remoteProvider.OnExtensionResponse += (sender, args) => OnExtensionResponse?.Invoke(sender, args);
         }
         protected virtual void Dispose(bool disposing)
         {
             if (disposing)
             {
-                provider.Dispose();
-                backupProvider?.Dispose();
+                remoteProvider.Dispose();
+                localProvider?.Dispose();
             }
         }
         public void Dispose()
@@ -74,13 +74,13 @@ namespace ScreenTimeClient.Configuration
 
         public Task RequestExtensionAsync(int v)
         {
-            return backupProvider.RequestExtensionAsync(v);
+            return localProvider.RequestExtensionAsync(v);
         }
 
         public async Task SendHeartbeatAsync(DateTimeOffset timestamp, TimeSpan duration)
         {
-            await backupProvider.SendHeartbeatAsync(timestamp, duration).ConfigureAwait(false);
-            await backupProvider.SendHeartbeatAsync(timestamp, duration).ConfigureAwait(false);
+            await localProvider.SendHeartbeatAsync(timestamp, duration).ConfigureAwait(false);
+            await remoteProvider.SendHeartbeatAsync(timestamp, duration).ConfigureAwait(false);
 
         }
     }
