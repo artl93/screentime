ffc25f284eec18b620187d8c3410df4008b10f19
Art Leonard <artl@microsoft.com>
2025-01-03 15:40:35 -0800

    Remove old database concepts.

--- src/ScreenTimeService/UserContext.cs
+++ src/ScreenTimeService/UserContext.cs
@@ -5,6 +5,6 @@ public class UserContext : DbContext
 {
     public UserContext(DbContextOptions<UserContext> options) : base(options) { }
 
-    public DbSet<UserEvent> UserEvents { get; set; }
-    public DbSet<UserConfiguration> UserConfigurations { get; set; }
+    // public DbSet<UserEvent> UserEvents { get; set; }
+    // public DbSet<UserConfiguration> UserConfigurations { get; set; }
 }
